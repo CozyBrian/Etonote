830e41b7248ff871182e148b6efaf5cfe400bff6
CozyBrian <dragonbrian33@gmail.com>
2022-09-02 16:36:56 +0000

    add: framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from "react";
-import "./App.css";
+import TasksView from "./components/Main/tasks.component";
+import SideBar from "./components/Sidebar/sidebar.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-3xl font-bold underline">Eton note taking</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-32 w-3/5 border-[3.5rem] border-sky-500 rounded-full mx-2 fixed left-0 -top-16 -z-10"></div>
+      <div className="h-screen w-full flex flex-row bg-slate-200/90 overflow-hidden backdrop-blur-xl z-30">
+        <SideBar />
+        <TasksView />
+      </div>
+    </>
   );
 }
 
