28add60d3f9d8fcfaf49d000a6a0b49a5c7a5cff
CozyBrian <dragonbrian33@gmail.com>
2022-09-04 17:37:47 +0000

    set-up config

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,8 @@
+// ./public/electron.js
 const path = require("path");
 
 const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
   // Create the browser window.
@@ -14,8 +16,15 @@ function createWindow() {
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
-  win.loadURL("http://localhost:3000");
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
   // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
 }
 
 // This method will be called when Electron has finished
@@ -24,7 +33,7 @@ function createWindow() {
 app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
+// for applications and their menu bars to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
