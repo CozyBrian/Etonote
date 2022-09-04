1d8f2ccabdab3ae20f0ec520953ed5ecef47598d
CozyBrian <dragonbrian33@gmail.com>
2022-09-04 03:09:16 +0000

    sidebar tiles

--- src/components/components/sidebar-item.component.tsx
+++ src/components/components/sidebar-item.component.tsx
@@ -19,22 +19,25 @@ const SideBarItem = ({
   const isSelected = appState.selectedTab === id;
 
   return (
-    <div
-      onClick={() => setTab(id)}
-      className={`w-full z-20 flex flex-row ${
-        isSelected
-          ? "bg-slate-400/30 [&>div:nth-child(2)]:bg-slate-300 backdrop-blur-xl hover:bg-slate-200 "
-          : "hover:bg-zinc-100 "
-      } [&>div:nth-child(2)]:hover:text-gray-800 [&>div:nth-child(2)]:active:bg-slate-300 active:bg-slate-200 cartnumber h-12 rounded-xl p-2 my-1 items-center duration-100 select-none justify-between`}
-    >
-      <div className="flex flex-row">
-        <div className="h-6 w-6 bg-slate-60 rounded-md mr-3 mx-2 items-center text-md justify-center">
-          {icon}
+    <div className="relative">
+      {isSelected && <div className="absolute top-3 text-2xl">{icon}</div>}
+      <div
+        onClick={() => setTab(id)}
+        className={`w-full z-20 flex flex-row ${
+          isSelected
+            ? "bg-slate-400/20 [&>div:nth-child(2)]:bg-slate-300 backdrop-blur-md hover:bg-slate-200 "
+            : "hover:bg-zinc-100 "
+        } [&>div:nth-child(2)]:hover:text-gray-800 [&>div:nth-child(2)]:active:bg-slate-300 active:bg-slate-200 cartnumber h-12 rounded-xl p-2 my-1 items-center duration-100 select-none justify-between`}
+      >
+        <div className="flex flex-row">
+          <div className="h-6 w-6 bg-slate-60 rounded-md mr-3 mx-2 items-center text-md justify-center">
+            {icon}
+          </div>
+          {title}
         </div>
-        {title}
-      </div>
-      <div className="text-sm text-gray-600 rounded-md bg-zinc-100 p-0.5 mx-2 duration-150">
-        {number}
+        <div className="text-sm text-gray-600 rounded-md bg-zinc-100 p-0.5 mx-2 duration-150">
+          {number}
+        </div>
       </div>
     </div>
   );
