26dbfcdddef45749bae153c0f6ab26b9c0f1c6eb
CozyBrian <dragonbrian33@gmail.com>
2022-09-07 03:56:27 +0000

    fix: import

--- src/components/components/loadingScreen.tsx
+++ src/components/components/loadingScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-// import image from "../../assets/images/splash.png";
+import image from "../../assets/images/splash.png";
 
 const LoadingScreen = () => {
   return (
@@ -11,11 +11,7 @@ const LoadingScreen = () => {
       className="absolute top-0 left-0 w-screen h-screen bg-slate-300 opacity-0 z-50 flex justify-center items-center"
     >
       <div className="flex justify-center flex-col">
-        <img
-          className="h-64"
-          src={require("../../assets/images/splash.png")}
-          alt="logo"
-        />
+        <img className="h-64" src={image} alt="logo" />
       </div>
     </motion.div>
   );
