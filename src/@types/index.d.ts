f2f0c2e6becdd3effa8e6142cef09d456f2e909d
CozyBrian <dragonbrian33@gmail.com>
2022-09-07 01:46:51 +0000

    electron-mac

---
+++ src/@types/index.d.ts
@@ -0,0 +1,13 @@
+export declare type SidebarItem = {
+  id: string;
+  title: string;
+  icon: string;
+};
+
+export declare type taskItem = {
+  id?: string;
+  listID: string;
+  title: string;
+  isDone: boolean;
+  icon: string;
+};
