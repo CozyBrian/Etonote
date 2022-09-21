2055868c6f60586b24c052fc440e27274a7a722a
CozyBrian <dragonbrian33@gmail.com>
2022-09-21 11:22:07 +0000

    add: deleteTodo

--- src/components/components/contextMenu.tsx
+++ src/components/components/contextMenu.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { action } from "../../redux";
+import { useAppDispatch } from "../../hooks";
 
 type Props = {
   id?: string;
@@ -7,25 +9,40 @@ type Props = {
 };
 
 const ContextMenu = ({ id, x, y }: Props) => {
+  const dispatch = useAppDispatch();
+
+  const toggleDone = (id: string | undefined) => {
+    return dispatch(action.todos.toggleDone(id));
+  };
+  const deleteTodo = (id: string | undefined) => {
+    return dispatch(action.todos.deleteTodo(id));
+  };
+
   const style = () => {
     return { top: y, left: x };
   };
   return (
     <div
       style={style()}
-      className={`absolute flex flex-col w-32 p-1 bg-slate-100 z-50 rounded-xl gap-1`}
+      className={`absolute flex flex-col w-48 p-0.5 bg-slate-100 z-50 rounded gap-1 shadow-md`}
     >
       <div
         onClick={() => console.log(id)}
-        className="flex flex-row h-8 w-full bg-slate-200/80 hover:bg-slate-300/80 active:bg-slate-300 text-black pl-2 p-2 items-center rounded-lg"
+        className="flex flex-row h-8 w-full bg-slate-200/80 hover:bg-slate-300/80 active:bg-slate-300 text-black pl-3 p-2 items-center rounded-sm"
       >
-        edit
+        Edit
       </div>
-      <div className="flex flex-row h-8 w-full bg-slate-200/80 hover:bg-slate-300/80 active:bg-slate-300 text-black pl-2 p-2 items-center rounded-lg">
-        copy
+      <div
+        onClick={() => toggleDone(id)}
+        className="flex flex-row h-8 w-full bg-slate-200/80 hover:bg-slate-300/80 active:bg-slate-300 text-black pl-3 p-2 items-center rounded-sm"
+      >
+        Check
       </div>
-      <div className="flex flex-row h-8 w-full bg-slate-200/80 hover:bg-slate-300/80 active:bg-slate-300 text-black pl-2 p-2 items-center rounded-lg">
-        paste
+      <div
+        onClick={() => deleteTodo(id)}
+        className="flex flex-row h-8 w-full bg-slate-200/80 hover:bg-slate-300/80 active:bg-slate-300 text-black pl-3 p-2 items-center rounded-sm"
+      >
+        Delete
       </div>
     </div>
   );
--- src/redux/todos-slice.ts
+++ src/redux/todos-slice.ts
@@ -56,6 +56,9 @@ const todos = createSlice({
         return item;
       });
     },
+    deleteTodo(state, action: PayloadAction<string | undefined>) {
+      state.value = state.value.filter((item) => item.id !== action.payload);
+    },
   },
 });
 
