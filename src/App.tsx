7cb7491a8fa941d9ad0d2154015b3b999258eef5
CozyBrian <dragonbrian33@gmail.com>
2022-09-24 20:00:21 +0000

    removed react-text-transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect } from "react";
 import TasksView from "./components/Main/tasks.component";
 import SideBar from "./components/Sidebar/sidebar.component";
-import TextTransition, { presets } from "react-text-transition";
 import { useAppSelector, useAppDispatch } from "./hooks";
 import LoadingScreen from "./components/components/loadingScreen";
 import { action } from "./redux";
 import { AnimatePresence } from "framer-motion";
+import BackIcon from "./components/components/backgroundIcon";
 
 function App() {
   const app = useAppSelector((state) => state.app);
-  const todoLists = useAppSelector((state) => state.lists.value);
   const dispatch = useAppDispatch();
-
-  const selectedList = todoLists.find((item) => item.id === app.selectedTab);
 
   useEffect(() => {
     setTimeout(() => {
@@ -23,15 +20,7 @@ function App() {
 
   return (
     <>
-      {app.selectedTab === app.homeId ? (
-        <div className="h-32 w-3/5 border-[3.5rem] border-sky-500 rounded-full mx-2 fixed left-0 -top-16 -z-10"></div>
-      ) : (
-        <div className="h-32 w-3/5 flex items-center justify-center text-[40vw] fixed left-0 -top-16 -z-10">
-          <TextTransition springConfig={presets.stiff}>
-            {selectedList?.icon}
-          </TextTransition>
-        </div>
-      )}
+      <BackIcon />
       <div className="relative h-screen w-screen flex flex-row bg-slate-200/90 overflow-hidden backdrop-blur-2xl z-30">
         <AnimatePresence>{app.isSplash && <LoadingScreen />}</AnimatePresence>
         <SideBar />
