95c1935fb28570dfbf41feb2860e3ee1e098ccf3
CozyBrian <dragonbrian33@gmail.com>
2022-09-24 00:18:22 +0000

    style: context menu and icons

--- src/components/Main/tasks.component.tsx
+++ src/components/Main/tasks.component.tsx
@@ -5,7 +5,7 @@ import TaskList from "./task-list.component";
 
 const TasksView = () => {
   return (
-    <div className="flex container justify-center p-12 pb-0">
+    <div className="flex container justify-center p-12 pb-0 select-none">
       <div className="min-w-[48rem]">
         <LayoutGroup>
           <Header />
