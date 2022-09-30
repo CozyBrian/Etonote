c8f6faecda337854a9fa6c25d4a153d9dafb2933
CozyBrian <dragonbrian33@gmail.com>
2022-09-30 04:42:17 +0000

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       className={global.THEME === "Dark" ? "dark duration-100" : "duration-100"}
     >
       <BackIcon />
-      <div className="relative h-screen w-screen flex flex-row bg-slate-200/90 dark:bg-zinc-900/90 overflow-hidden backdrop-blur-2xl z-30">
+      <div className="relative h-screen w-screen flex flex-row bg-slate-200/90 dark:bg-zinc-900/95 overflow-hidden backdrop-blur-2xl z-30">
         <AnimatePresence>{app.isSplash && <LoadingScreen />}</AnimatePresence>
         <SideBar />
         <TasksView />
