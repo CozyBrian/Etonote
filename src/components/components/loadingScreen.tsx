f2d7c06a279f2e153da0433020c15116d3be8622
CozyBrian <dragonbrian33@gmail.com>
2022-10-02 21:20:11 +0000

    style: dark mode splash screen

--- src/components/components/loadingScreen.tsx
+++ src/components/components/loadingScreen.tsx
@@ -8,11 +8,11 @@ const LoadingScreen = () => {
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="absolute top-0 left-0 w-screen h-screen bg-slate-300 opacity-0 z-50 flex justify-center items-center"
+      className="absolute top-0 left-0 w-screen h-screen bg-slate-300 dark:bg-zinc-900 opacity-0 z-50 flex justify-center items-center"
     >
       <div className="flex justify-center flex-col">
         <img
-          className="h-64"
+          className="h-48"
           src={require("../../assets/images/splash.png")}
           alt="logo"
         />
