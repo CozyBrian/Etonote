e8180b705eeebbfee702d5a551a8570f0238021a
CozyBrian <dragonbrian33@gmail.com>
2022-12-11 23:51:58 +0000

    realtimey-ness

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,27 @@ import TasksView from "./components/Main/tasks.component";
 import SideBar from "./components/Sidebar/sidebar.component";
 import { motion } from "framer-motion";
 import BackIcon from "./components/components/backgroundIcon";
+// import { getDatabase, ref, onValue } from "firebase/database";
+// import { useAppDispatch, useAppSelector } from "./hooks";
+// import { action } from "./redux";
+// import { useEffect } from "react";
 
 function App() {
+  // const User = useAppSelector((state) => state.user);
+  // const dispatch = useAppDispatch();
+
+  // const db = getDatabase();
+  // const userStateRef = ref(db, "users/" + User.user_id + "/state");
+
+  // useEffect(() => {
+  //   onValue(userStateRef, (snapshot) => {
+  //     const data = snapshot.val();
+  //     dispatch(action.system.setState(data.system));
+  //     dispatch(action.lists.setState(data.lists));
+  //     dispatch(action.todos.setState(data.todos));
+  //   });
+  // }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
